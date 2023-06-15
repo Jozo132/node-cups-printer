e8844f43988f15a1fa55166ffdf138cd2d67b462
J.Vovk <jozo132@gmail.com>
2023-06-15 16:04:24 +0200

    Added more printer info

--- vovk-cups-printer.js
+++ vovk-cups-printer.js
@@ -92,7 +92,7 @@ const runCommandPipe = (command, input, ...args) => new Promise((resolve, reject
  * }} PrintDirectOptions
 */
 
-/** @typedef { (jobId: string) => any } PrintOnSuccessFunction */
+/** @typedef { (jobId: number) => any } PrintOnSuccessFunction */
 /** @typedef { (err: Error) => any } PrintOnErrorFunction */
 
 
@@ -113,54 +113,61 @@ const runCommandPipe = (command, input, ...args) => new Promise((resolve, reject
  *     copies?: number | undefined
  *     args?: string[] | undefined
  * }} PrintOptions 
+ * @returns { Promise<number> }
  */
 const lp_print = (/**@type {PrintOptions}*/options) => new Promise((resolve, reject) => {
-    const args = options.args || []
-    const printer = options.printer
-    const type = ((options.type || 'RAW') + '').toLowerCase() // [RAW, ...]
-    const data = options.data
-    const file = options.file
-    const host = options.host ? [options.host, options.port ? options.port : ''].filter(Boolean).join(':') : ''
-    const username = options.username
-    const encryption = options.encryption || false
-    const title = options.title
-    const quality = options.quality
-    const orientation = options.orientation
-    const copies = options.copies
+    try {
+        const args = options.args || []
+        const printer = options.printer
+        const type = ((options.type || 'RAW') + '').toLowerCase() // [RAW, ...]
+        const data = options.data
+        const file = options.file
+        const host = options.host ? [options.host, options.port ? options.port : ''].filter(Boolean).join(':') : ''
+        const username = options.username
+        const encryption = options.encryption || false
+        const title = options.title
+        const quality = options.quality
+        const orientation = options.orientation
+        const copies = options.copies
 
-    if (!printer) throw new Error('No printer provided')
-    if (!data && !file) throw new Error('No data or file provided')
+        if (!printer) throw new Error('No printer provided')
+        if (!data && !file) throw new Error('No data or file provided')
 
-    if (!file) args.push('-o', type)
-    if (copies) args.push(`-n`, copies + '')
-    if (printer) args.push(`-d`, printer)
-    if (host) args.push(`-h`, host)
-    if (username) args.push(`-U`, username)
-    if (title) args.push(`-T`, title)
-    if (quality) args.push(`-o`, `print-quality=${quality}`)
-    if (orientation) args.push(`-o`, `orientation-requested=${orientation}`)
-    if (encryption) args.push(`-E`)
+        if (!file) args.push('-o', type)
+        if (copies) args.push(`-n`, copies + '')
+        if (printer) args.push(`-d`, printer)
+        if (host) args.push(`-h`, host)
+        if (username) args.push(`-U`, username)
+        if (title) args.push(`-T`, title)
+        if (quality) args.push(`-o`, `print-quality=${quality}`)
+        if (orientation) args.push(`-o`, `orientation-requested=${orientation}`)
+        if (encryption) args.push(`-E`)
 
-    const callback = response => {
-        const split = response.split(' ').filter(Boolean)
-        const jobId = split.length > 3 ? split[split.length - 3] : response
-        resolve(jobId)
+        /** @type { (response: string) => void } */
+        const callback = response => {
+            const split = response.split(' ').filter(Boolean)
+            const jobIdLong = split.length > 3 ? split[split.length - 3] : response
+            const jobId = +jobIdLong.split('-')[jobIdLong.split('-').length - 1]
+            resolve(jobId)
+        }
+
+        if (file) {
+            args.push('--', file)
+            runCommand('lp', ...args).then(callback)
+            return
+        }
+
+        if (data) {
+            runCommandPipe('lp', data, ...args).then(callback)
+            return
+        }
+        throw new Error('No data or file provided')
+    } catch (err) {
+        reject(err)
     }
-
-    if (file) {
-        args.push('--', file)
-        runCommand('lp', ...args).then(callback)
-        return
-    }
-
-    if (data) {
-        runCommandPipe('lp', data, ...args).then(callback)
-        return
-    }
-    throw new Error('No data or file provided')
 })
 
-/** @param { PrintDirectOptions } options */
+/** @type { (options: PrintDirectOptions) => void} */
 const printDirect = options => {
     const { data, printer, type, options: optionsObj, success, error } = options
     if (!error) throw new Error('No error callback provided')
@@ -185,9 +192,33 @@ const printDirect = options => {
     }
 }
 
-/** @type { (input: string) => PrinterDetails[] } */
-const extractPrinters = input => {
-    const lines = input.split('\n')
+/** @typedef {{ [key: string]: [string, string] | [string, string, string] }} LPSTAT_in */
+/** @typedef {{ [key: string]: string }} LPSTAT_out */
+
+/** @type { LPSTAT_in } */
+const lpstat_commands = {
+    printers: ['lpstat', '-p'], // Printers and their status
+    accepting: ['lpstat', '-a'], // Printers accepting requests
+    addresses: ['lpstat', '-s'], // Printers and their addresses
+    default: ['lpstat', '-d'], // Default printer
+    details: ['lpstat', '-l', '-p'], // Printer details
+}
+
+const lpstat_keys = Object.keys(lpstat_commands)
+
+
+const lpstat_detail_ki = [
+    { key: 'printer-info', filter: 'Description: ' },
+    { key: 'printer-location', filter: 'Location: ' },
+]
+
+/** @type { (stats: LPSTAT_out) => PrinterDetails[] } */
+const extractPrinters = stats => {
+    const { accepting, default: defaultPrinter, addresses, printers, details } = stats
+    const addressLines = addresses.split('\n')
+    const acceptingLines = accepting.split('\n')
+    const lines = printers.split('\n')
+    const printerDetails = details.split('\nprinter').filter(Boolean)
     /** @type { PrinterDetails[] } */
     const result = []
     /** @type { PrinterDetails | null } */
@@ -195,10 +226,62 @@ const extractPrinters = input => {
     for (const line of lines) {
         const words = line.split(' ')
         if (words[0] === 'printer') {
-            currentPrinter = {
-                name: words[1],
+            const name = words[1]
+            const isDefault = defaultPrinter.includes(name)
+            const socket_line = addressLines.find(line => line.includes(name + ': '))
+            const socket = socket_line ? socket_line.split(' ')[socket_line.split(' ').length - 1] : ''
+            const acceptingRequests = !!acceptingLines.find(line => line.includes(name + ' accepting')) ? 'true' : 'false'
+            const details = printerDetails.find(line => line.includes(name + ' '))
+            const info = {}
+
+            if (details) {
+                const rows = details.split('\n').map(line => line.trim()).filter(Boolean)
+                for (let i = 0; i < lpstat_detail_ki.length; i++) {
+                    const { key, filter } = lpstat_detail_ki[i]
+                    const row = rows.find(row => row.startsWith(filter))
+                    if (row) {
+                        info[key] = row.slice(filter.length).trim()
+                    }
+                }
+            }
+
+            /* // Actual printer details from the 'node-printer' package
+                name: 'ZPL-printer-test',
                 isDefault: false,
-                options: {}
+                options: {
+                    copies: '1',
+                    'device-uri': 'socket://192.168.1.40:9100',
+                    finishings: '3',
+                    'job-cancel-after': '10800',
+                    'job-hold-until': 'no-hold',
+                    'job-priority': '50',
+                    'job-sheets': 'none,none',
+                    'marker-change-time': 1970-01-01T00:00:00.000Z,
+                    'number-up': '1',
+                    'print-color-mode': 'monochrome',
+                    'printer-commands': 'none',
+                    'printer-info': 'Toshiba Label Printer (ZPL format)',
+                    'printer-is-accepting-jobs': 'true',
+                    'printer-is-shared': 'false',
+                    'printer-is-temporary': 'false',
+                    'printer-location': 'XT2',
+                    'printer-make-and-model': 'TOSHIBA B-EX4T2-G',
+                    'printer-state': '3',
+                    'printer-state-change-time': 2023-06-09T11:37:35.000Z,
+                    'printer-state-reasons': 'none',
+                    'printer-type': '1234567',
+                    'printer-uri-supported': 'ipp://localhost/printers/ZPL-printer-test'
+                },
+                status: 'IDLE'
+            */
+            currentPrinter = {
+                name,
+                isDefault,
+                options: {
+                    'printer-is-accepting-jobs': acceptingRequests,
+                    'device-uri': socket,
+                    ...info,
+                }
             }
         }
         if (currentPrinter) result.push(currentPrinter)
@@ -222,14 +305,28 @@ class Printer {
 
     /** @returns { PrinterDetails[] } */
     getPrinters() {
+        /** @type { LPSTAT_out } */
+        const stats = {}
         if (!this.printersLoaded) {
-            const printers = runCommandSync(`lpstat`, `-p`, `-d`)
-            this.printers = extractPrinters(printers)
+            lpstat_keys.forEach(key => {
+                const command = lpstat_commands[key] // @ts-ignore
+                const response = runCommandSync(...command)
+                stats[key] = response
+            })
+            // const printers = runCommandSync(`lpstat`, `-p`, `-d`)
+            this.printers = extractPrinters(stats)
             this.printersLoaded = true
             return this.printers
-        }
-        runCommand(`lpstat`, `-p`, `-d`).then(response => {
-            this.printers = extractPrinters(response)
+        } // @ts-ignore
+        Promise.all(lpstat_keys.map(key => runCommand(...lpstat_commands[key]))).then(responses => {
+            /** @type { LPSTAT_out } */
+            const stats = {}
+            for (let i = 0; i < responses.length; i++) {
+                const key = lpstat_keys[i]
+                const response = responses[i]
+                stats[key] = response
+            }
+            this.printers = extractPrinters(stats)
             this.printersLoaded = true
         })
         return this.printers
@@ -251,7 +348,7 @@ class Printer {
         printDirect(options)
     }
 
-    /** @param {PrintOptions} options * @returns { Promise<string> } */
+    /** @param {PrintOptions} options * @returns { Promise<number> } */
     print(options) {
         return new Promise((resolve, reject) => {
             const { printer } = options
